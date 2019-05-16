83ed3fe300e373ea73d85354121dce39c93b0640
houyafei <houyafei@wsdashi.com>
2019-05-16 20:23:20 +0800

    change file

--- src/main/scala/tencentsms/util/TencentTestKey.scala
+++
@@ -1,9 +0,0 @@
-package tencentsms.util
-
-object TencentTestKey {
-  //测试
-    val appKey = "c048470f0e608c0535214b26b1617edc"
-    val sdkappid = "1400184284"
-  //  val tmpId = 314223
-  //  278747,78749
-}
